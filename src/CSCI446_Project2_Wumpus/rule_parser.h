cd3a1338f3b638016d76aeb9b6eea0da33b8d956
byrdie <byrdie@bootes>
2016-10-16 17:27:38 -0600

    Fixed typedefs a little

--- src/CSCI446_Project2_Wumpus/rule_parser.h
+++ src/CSCI446_Project2_Wumpus/rule_parser.h
@@ -22,12 +22,16 @@
 
 
 using namespace std;
-typedef vector <vector<tuple<int, vector<tuple<int, vector<int> > > > > >  cnf;
-typedef vector<tuple<int, vector<tuple<int, vector<int> > > > >         clause;
-typedef tuple<int, vector<tuple<int, vector<int> > > >                    pred;
-typedef vector<tuple<int, vector<int> > >                                funcs;
-typedef tuple<int, vector<int> >                                          func;
-typedef vector<int>                                                        var;
+//typedef vector <vector<tuple<int, vector<tuple<int, vector<int> > > > > >  cnf;
+//typedef vector<tuple<int, vector<tuple<int, vector<int> > > > >         clause;
+//typedef tuple<int, vector<tuple<int, vector<int> > > >                    pred;
+//typedef vector<tuple<int, vector<int> > >                                funcs;
+//typedef tuple<int, vector<int> >                                          func;
+typedef vector<int>             func_args;  // integer represents function parameters, variable index, or constant value
+typedef tuple<int, func_args>   func;       // integer represents function name, variable type, or constant type
+typedef vector<func>            pred_args;  // Arguments to predicates
+typedef tuple<int, pred_args>   pred;       // Integer represents predicate name
+typedef vector<vector<pred>>    cnf;        // CNF representation of all predicates
 
 void parse();
 #endif /* RULE_PARSER_H */
