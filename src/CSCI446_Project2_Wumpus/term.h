ec06603e2ac1607c151ff730d06947a7a3e207b5
byrdie <roytsmart@gmai.com>
2016-10-16 00:39:15 -0400

    Still trying to figure out knowledge base

--- src/CSCI446_Project2_Wumpus/term.h
+++ src/CSCI446_Project2_Wumpus/term.h
@@ -14,6 +14,8 @@
 #ifndef TERM_H
 #define TERM_H
 
+#include <iostream>
+
 #include "typedef.h"
 
 using namespace std;
@@ -32,14 +34,14 @@ public:
     virtual Point * operator()();    // General vec evaluation function
 };
 
-class ConstVec : public Vec {
+class Const : public Vec {
 public:
     Const(Point * pt);
-    type operator()();
+    virtual Point * operator()();
     
 };
 
-class VarVec : public Vec {
+class Var : public Vec {
 public:
     Var();
     virtual Point * operator()();
@@ -48,24 +50,28 @@ public:
 
 class North : public Vec {
 public:
+    Vec * inp;
     North(Vec * input);
     virtual Point * operator()();
 };
 
 class South : public Vec {
 public:
+    Vec * inp;
     South(Vec * input);
     virtual Point * operator()();
 };
 
 class East : public Vec {
 public:
+    Vec * inp;
     East(Vec * input);
     virtual Point * operator()();
 };
 
 class West : public Vec {
 public:
+    Vec * inp;
     West(Vec * input);
     virtual Point * operator()();
 };
