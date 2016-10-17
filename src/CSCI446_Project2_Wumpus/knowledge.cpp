93c262a130c6aabfecf274ac2078839981b6f357
byrdie <roytsmart@gmai.com>
2016-10-16 23:25:22 -0400

    Added maps for functions and predicates.

--- src/CSCI446_Project2_Wumpus/knowledge.cpp
+++ src/CSCI446_Project2_Wumpus/knowledge.cpp
@@ -1,8 +1,21 @@
 
 #include "knowledge.h"
 
-Knowledge::Knowledge(){
-    
-    
-    
+Knowledge::Knowledge() {
+
+
+
+}
+
+void initialize_token_map() {
+    pred_tok_map["Breezy"] = P_BREEZY;
+    pred_tok_map["Pit"] = P_PIT;
+    pred_tok_map["Safe"] = P_SAFE;
+
+
+    func_tok_map["North"] = F_NORTH;
+    func_tok_map["South"] = F_SOUTH;
+    func_tok_map["East"] = F_EAST;
+    func_tok_map["West"] = F_WEST;
+   
 }
